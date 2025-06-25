aedda71979f0cd7acfb5e7b4df20cbfabc7e0cc8
Ryszard Kluz <Ryszard.kluz.fm@gmail.com>
2025-06-25 11:11:44 +0200

    fix(getKeys): fix key filtering logic and improve control flow

--- client/src/helpers/getKeys.ts
+++ client/src/helpers/getKeys.ts
@@ -4,12 +4,14 @@ const getKeys = <T extends Resource>(
   resourceList: T[],
   isOrderDetailsList: boolean,
 ): (keyof T)[] => {
+  if (isOrderDetailsList) {
+  }
   if (!resourceList || resourceList.length === 0) {
     throw new Error('No list entries !');
   }
   const keys = Object.keys(resourceList[0]) as (keyof T)[];
   const filteredKeys = isOrderDetailsList
-    ? keys.filter((key) => key !== 'productId' && key !== 'count')
+    ? keys.filter((key) => key !== 'productCount')
     : keys;
 
   return filteredKeys;
