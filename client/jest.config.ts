77370776c008df5a4b38474c6a5603c833656c61
Ryszard Kluz <Ryszard.kluz.fm@gmail.com>
2025-07-15 14:15:28 +0200

    refactor: -delete unnecessary config file -instal swc/jest to speed up testing -update config and create jest.setup to properly use jest testing library

--- client/jest.config.ts
+++ client/jest.config.ts
@@ -1,1 +1,13 @@
-import '@testing-library/jest-dom';
+import type { Config } from 'jest';
+
+const config: Config = {
+  preset: 'ts-jest',
+  testEnvironment: 'jest-environment-jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  cache: true,
+  transform: {
+    '^.+\\.(t|j)sx?$': '@swc/jest',
+  },
+};
+
+export default config;
